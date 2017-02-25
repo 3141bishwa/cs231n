Fixed softmax and completed neural network portion of the assignment

assignment1/cs231n/classifiers/neural_net.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from softmax import softmax
 
 class TwoLayerNet(object):
   """
@@ -67,8 +67,13 @@ class TwoLayerNet(object):
     W2, b2 = self.params['W2'], self.params['b2']
     N, D = X.shape
 
+    h1 = np.dot(X,W1) + b1
+    
+    
     # Compute the forward pass
-    scores = None
+    scores_1 = np.maximum(0,h1)
+    
+    scores = np.dot(scores_1, W2) + b2
     #############################################################################
     # TODO: Perform the forward pass, computing the class scores for the input. #
     # Store the result in the scores variable, which should be an array of      #
@@ -83,8 +88,9 @@ class TwoLayerNet(object):
     if y is None:
       return scores
 
+    
     # Compute the loss
-    loss = None
+    loss = 0.0
     #############################################################################
     # TODO: Finish the forward pass, and compute the loss. This should include  #
     # both the data loss and L2 regularization for W1 and W2. Store the result  #
@@ -92,7 +98,16 @@ class TwoLayerNet(object):
     # classifier loss. So that your results match ours, multiply the            #
     # regularization loss by 0.5                                                #
     #############################################################################
-    pass
+    out = softmax(scores)
+    
+    score_y_i = out[xrange(N), y]
+    data_loss = np.sum(-np.log(score_y_i))
+
+    data_loss /= N
+    regularization_loss = 0.5 * reg * (np.sum(W1 * W1) + np.sum(W2 * W2))
+    loss = data_loss + regularization_loss
+    
+    
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -104,7 +119,24 @@ class TwoLayerNet(object):
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     #############################################################################
-    pass
+    dscores = out
+    dscores[(xrange(N), y)] -= 1
+    dscores /= N
+    
+    
+    grads["W2"] = np.dot(scores_1.T , dscores)
+    grads['b2'] = np.sum(dscores, axis=0)
+    
+    dhidden = np.dot(dscores, W2.T)
+    
+    dhidden[scores_1 <= 0] = 0
+    
+    grads['W1'] = np.dot(X.T, dhidden)
+    grads['b1'] = np.sum(dhidden, axis=0)
+    
+    
+    grads['W2'] += reg * W2
+    grads['W1'] += reg * W1
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -141,29 +173,29 @@ class TwoLayerNet(object):
     val_acc_history = []
 
     for it in xrange(num_iters):
-      X_batch = None
-      y_batch = None
-
-      #########################################################################
-      # TODO: Create a random minibatch of training data and labels, storing  #
-      # them in X_batch and y_batch respectively.                             #
-      #########################################################################
-      pass
-      #########################################################################
-      #                             END OF YOUR CODE                          #
-      #########################################################################
+      if num_train < batch_size:
+        batch_size = num_train
+      
+      choices = np.random.choice(num_train, (batch_size,), replace=False)
+      
+      X_batch = X[choices]
+      y_batch = y[choices]
 
       # Compute loss and gradients using the current minibatch
       loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
       loss_history.append(loss)
-
+      
       #########################################################################
       # TODO: Use the gradients in the grads dictionary to update the         #
       # parameters of the network (stored in the dictionary self.params)      #
       # using stochastic gradient descent. You'll need to use the gradients   #
       # stored in the grads dictionary defined above.                         #
       #########################################################################
-      pass
+      
+      self.params['W1'] -= learning_rate * grads['W1']
+      self.params['b1'] -= learning_rate * grads['b1']
+      self.params['W2'] -= learning_rate * grads['W2']
+      self.params['b2'] -= learning_rate * grads['b2']
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -208,11 +240,20 @@ class TwoLayerNet(object):
     ###########################################################################
     # TODO: Implement this function; it should be VERY simple!                #
     ###########################################################################
-    pass
+    W1, b1 = self.params['W1'], self.params['b1']
+    W2, b2 = self.params['W2'], self.params['b2']
+    N, D = X.shape
+
+    h1 = np.dot(X,self.params['W1']) + self.params['b1']
+    
+    # Compute the forward pass
+    scores_1 = np.maximum(0,h1)
+    
+    scores = np.dot(scores_1, self.params['W2']) + self.params['b2']
     ###########################################################################
     #                              END OF YOUR CODE                           #
     ###########################################################################
 
-    return y_pred
+    return np.argmax(softmax(scores), axis = 1)
 
 

assignment1/cs231n/classifiers/softmax.py
@@ -38,43 +38,23 @@ def softmax_loss_naive(W, X, y, reg):
         # Compute vector of scores
         f_i = X[i].dot(W)
         
-
         out = softmax(f_i)
 
-        diff = np.exp(out - np.max(out))
-
-        sum_diff = np.sum(diff)
-
-        score = diff[y[i]]/sum_diff
-
-        loss += - np.log(score)
-
-        f_i -= np.max(f_i)
+        sum_out = np.sum(out)
         
-        # Compute gradient
-        # Here we are computing the contribution to the inner sum for a given i.
+        loss += - np.log(out[y[i]])
+
         for k in range(num_classes):
-            #print np.exp(f_i[k])/(np.sum(np.exp(f_i)))
-            #print out[y[k]]
-            #print ""
-            score_k = np.exp(f_i[k])/(np.sum(np.exp(f_i)))
-            #print score_k
-            #print score_k
-            #print out[k]
-                                      
+
+            score_k = out[k]/sum_out                              
             dW[:,k] += X[i]*(-1*(k==y[i]) + score_k)
     
     loss /= num_train
     loss += 0.5 * reg * np.sum(W * W)
     dW /= num_train
     dW += reg*W
-    #############################################################################
-    #                          END OF YOUR CODE                                 #
-    #############################################################################
 
-    #print dW
     return loss, dW
-
 
 def softmax_loss_vectorized(W, X, y, reg):
     """
@@ -88,30 +68,27 @@ def softmax_loss_vectorized(W, X, y, reg):
     num_examples = X.shape[0]
     scores = np.dot(X, W)
     
-    probs = softmax(scores)
-    corect_logprobs = -np.log(probs[range(num_examples),y])
+    out = softmax(scores)
     
-    data_loss = np.sum(corect_logprobs)/num_examples
-    reg_loss = 0.5*reg*np.sum(W*W)
+    score_i = out[xrange(num_examples), y]
+    loss = np.sum(-np.log(score_i))
+
+    out[xrange(num_examples), y] -= 1
+    dW = np.dot(X.T,out)
     
-    loss = data_loss + reg_loss
-    
-    print loss
-    dscores = probs
-    dscores[range(num_examples),y] -= 1
-    dscores /= num_examples
-    dW = np.dot(X.T, dscores)
-    
+
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
     # Store the loss in loss and the gradient in dW. If you are not careful     #
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization!                                                           #
     #############################################################################
-    pass
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
+    loss /= num_examples
+    loss += 0.5 * reg * np.sum(W * W)
+    dW /= num_examples
     dW += reg*W
     
     return loss, dW