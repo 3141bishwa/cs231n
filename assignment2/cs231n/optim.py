c48af2d98b110af9d256055404b008683bd36a2b
bsilwal <bishwa.slwl@gmail.com>
2017-03-08 05:09:55 -0500

    Fixed implementation of Adam update rule

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -141,9 +141,17 @@ def adam(x, dx, config=None):
   # stored in config.                                                         #
   #############################################################################
   next_x = x.copy()
+
+  # Incrementing t by 1 ensures that beta ** t is not 0 when t = 0
+  # which causes a division by zero problem when calculating mb and vb below.
+  config["t"] += 1
   config["m"] = config["beta1"]*config["m"] + (1-config["beta1"])*dx
-  config["v"] =  config["beta2"]* config["v"] + (1- config["beta2"])*(dx**2)
-  next_x += -  config["learning_rate"] * config["m"] / (np.sqrt(config["beta1"]) + config["epsilon"])
+  config["v"] = config["beta2"]* config["v"] + (1- config["beta2"])*(dx**2)
+
+  mb = config["m"]/(1 - config["beta1"]**config["t"])
+  vb = config["v"]/(1 - config["beta2"]**config["t"])
+    
+  next_x += -  config["learning_rate"] * mb / (np.sqrt(vb) + config["epsilon"])
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
